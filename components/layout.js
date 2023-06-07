7c3f6ceb8ded7b95b3307326facac713bbf5b9a2
HP <thathsara.hewage99@gmail.com>
2023-06-07 15:44:26 +0530

    admin login function

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+// import { useSession } from "next-auth/react";
+
+// export default function Layout() {
+//     const { data: session } = useSession();
+//     if (!session) {
+//         return (
+//             <div>
+//                 <div>
+//                     <button onClick={ }></>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,16 @@ module.exports = function Home() {
     if (!session) {
         return (
             <main>
-                <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
-                    <div className={"text-center w-full"}>
-                        <button className={"bg-white p-2 text-black px-4 rounded-lg"}>Login with Google</button>
+                <div className="bg-blue-900 w-screen h-screen flex items-center">
+                    <div className="text-center w-full">
+                        <button onClick={() => signIn()} className="bg-white p-2 text-black px-4 rounded-lg">Login with Google</button>
                     </div>
                 </div>
             </main>
         )
     }
+
+    return (
+        <div>logged in {session.user.email}</div>
+    );
 }
